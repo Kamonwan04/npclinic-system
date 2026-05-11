f5b946d558cf4c488109068dc96d947ee347ac4c
NP STAFF <npstaff@NPs-Mac-mini.local>
2026-05-11 18:08:47 +0700

    fix railway port

--- backend/app.js
+++ backend/app.js
@@ -659,6 +659,9 @@ app.post('/df-records', (req, res) => {
 
 });
 // ------------------------------------
-app.listen(3001, () => {
-  console.log('✅ Server running on port 3001');
+// ------------------------------------
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
